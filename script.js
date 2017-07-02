d97a39c31bf7863f406e9e2872009241d1e711fd
boazsd <boazsd@gmail.com>
2017-07-02 12:54:08 +0300

    Targil 1

--- script.js
+++ script.js
@@ -4,4 +4,3 @@ x="boaz shlomo-david"
 console.log( typeof x);
 var myObject = {eyeColor:"blue", name:"Fiat", height: 180};
 myObject.eyeColor = "red";
-var myArray =[2, 4, 6, 8, 10, 12, 14, 16, 18, 20]; 
