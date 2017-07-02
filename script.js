f1b4e1cad26672bc865c2020a55347751d87330c
boazsd <boazsd@gmail.com>
2017-07-02 12:41:46 +0300

    targil 1

---
+++ script.js
@@ -0,0 +1,7 @@
+var x=3;
+console.log(x);
+x="boaz shlomo-david"
+console.log( typeof x);
+var myObject = {eyeColor:"blue", name:"Fiat", height: 180};
+myObject.eyeColor = "red";
+var myArray =[2, 4, 6, 8, 10, 12, 14, 16, 18, 20]; 
