ae51487ec775cfa656e93794abdf729e7db2d2ba
boazsd <boazsd@gmail.com>
2017-07-02 13:47:52 +0300

    update targil 2

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ x="boaz shlomo-david"
 console.log( typeof x);
 var myObject = {eyeColor:"blue", name:"Fiat", height: 180};
 myObject.eyeColor = "red";
+myObject.isInIsrael = true;
+myObject.city = "tel-Aviv";
+var languages = {Hebrew: true, English: true, Russian: false}
+myObject.languages = languages;
+console.log( myObject.languages.Hebrew);
+var myArray =[2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+console.log( myArray);
+console.log( myArray.length);
+delete myArray[9];
+delete myArray[8];
+console.log( myArray);
